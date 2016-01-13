cd15dec03ab9e2807800a85176779d6bf58f625d
Andrew <andrew.mitchell@zentri.com>
2016-01-13 12:36:16 +1100

    Add retries to asset management to make recovery 'less aggressive'
    Conflicts:
    	public/views/js/index.js

--- public/views/js/index.js
+++ public/views/js/index.js
@@ -1,16 +1,25 @@
 var assetsLoaded = 0;
-var assets = [
-  {pos: 'head', tag: 'link', attrs: {rel: 'stylesheet', type: 'text/css', href: '/webapp/#{path}zentrios.css'}},
-  {pos: 'body', tag: 'script', attrs: { type: 'text/javascript', src: '/webapp/#{path}zentrios.js'}}
-];
-for(var i = 0; i < assets.length; i++) {
-  var asset = document.createElement(assets[i].tag);
-  for(var j = 0; j < Object.keys(assets[i].attrs).length; j++) {
-    asset[Object.keys(assets[i].attrs)[j]] = assets[i].attrs[Object.keys(assets[i].attrs)[j]];
+var assets = {
+  '/webapp/#{path}wiconnect.css': {pos: 'head', tag: 'link', attrs: {rel: 'stylesheet', type: 'text/css', href: '/webapp/#{path}wiconnect.css'}, attempt: 1},
+  '/webapp/#{path}wiconnect.js': {pos: 'body', tag: 'script', attrs: { type: 'text/javascript', src: '/webapp/#{path}wiconnect.js'}, attempt: 1}
+};
+function loadAsset(asset) {
+  asset.el = document.createElement(asset.tag);
+  for(var j = 0; j < Object.keys(asset.attrs).length; j++) {
+    asset.el[Object.keys(asset.attrs)[j]] = asset.attrs[Object.keys(asset.attrs)[j]];
   }
-  document[assets[i].pos].appendChild(asset);
-  asset.addEventListener('load', assetLoad);
-  asset.addEventListener('error', assetErr);
+  document[asset.pos].appendChild(asset.el);
+  asset.el.addEventListener('load', assetLoad);
+  asset.el.addEventListener('error', assetErr);
 }
 function assetLoad(e){assetsLoaded++;if(assetsLoaded === assets.length && (typeof _webapp === 'undefined')){top.location = '/.recovery.html';}}
-function assetErr(e){top.location = '/.recovery.html';}
+function assetErr(e){
+  var asset = assets[(e.target.src||e.target.href).replace(window.location.origin,'')];
+  if(asset.attempt < 3) {
+    asset.attempt++;
+    document[asset.pos].removeChild(this);
+    return setTimeout(loadAsset, 1000, asset);
+  }
+  top.location = '/.recovery.html';
+}
+Object.keys(assets).forEach(function(asset){loadAsset(assets[asset])});
