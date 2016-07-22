5bc374273176d98503494e13994ff12d7b1c602f
Andrew <andrew.mitchell@zentri.com>
2016-07-22 15:56:51 +1000

    fix console

--- public/js/views/console.js
+++ public/js/views/console.js
@@ -117,7 +117,7 @@ App.Views.Console = Backbone.View.extend({
 
     self.output = $(self.el).find('output')[0];
 
-    self.printLine('ZentriOS Web App Console - v' + _webapp.version);
+    self.printLine('ZentriOS-WL Web App Console - v' + _webapp.version);
 
     if(self.device.commands.length > 0 && Object.keys(self.device.variables).length > 0){
       //commands and variables already loaded, nothing to do
@@ -127,7 +127,7 @@ App.Views.Console = Backbone.View.extend({
     async.waterfall([
       function(next) {
         self.printLine('loading commands...');
-        self.device.wiconnect.help({args: 'commands'}, function(err, res){
+        self.device.zentrios.help({args: 'commands'}, function(err, res){
           if(err) {
             return next(err);
           }
@@ -136,7 +136,7 @@ App.Views.Console = Backbone.View.extend({
       },
       function(next) {
         self.printLine('loading variables...');
-        self.device.wiconnect.help({args: 'variables'}, function(err, res){
+        self.device.zentrios.help({args: 'variables'}, function(err, res){
           if(err) {
             return next(err);
           }
@@ -376,7 +376,7 @@ App.Views.Console = Backbone.View.extend({
           args = args.join(' ');
           re = /(\'(.+)\')|(\"(.+)\")/;
           var alias = args.substring(0, args.indexOf('='));
-          if(self.device.wiconnect.hasOwnProperty(alias) || self.cmdMask.hasOwnProperty(alias)) {
+          if(self.device.zentrios.hasOwnProperty(alias) || self.cmdMask.hasOwnProperty(alias)) {
             self.printOutput(['Invalid alias name']);
             break;
           }
@@ -565,7 +565,7 @@ App.Views.Console = Backbone.View.extend({
     var self = this;
 
     setTimeout(function(){
-      self.device.wiconnect.ver(
+      self.device.zentrios.ver(
         {retries: 1, timeout: self.reconnect.timeout},
         function(err, res) {
           if(err){
